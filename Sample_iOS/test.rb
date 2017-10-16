7726fda257d555576ce171ec86ddba9dbf1b6a01
WataruSuzuki <wataru0406@gmail.com>
2017-10-16 21:30:00 +0900

    Modified test to using rspec

--- Sample_iOS/test.rb
+++ Sample_iOS/test.rb
@@ -1,12 +1,17 @@
 require 'rubygems'
 require 'appium_lib'
+require 'rspec'
+
+RSpec.configure do |c|
+  # c.treat_symbols_as_metadata_keys_with_true_values = true
+end
 
 APP_PATH = 'appium-unittesting/Debug-iphonesimulator/Sample_iOS.app'
 
 desired_caps = {
   caps: {
     platformName:  'iOS',
-    platformVersion: '11.0',
+    platformVersion: '11.1',
     deviceName:    'iPhone X',
     app:           APP_PATH,
     automationName: 'Appium',
@@ -18,13 +23,27 @@ desired_caps = {
   }
 }
 
-# Start the driver
-Appium::Driver.new(desired_caps, true).start_driver
+describe 'Sample_iOS' do
+    before(:all) do
+        Appium::Driver.new(desired_caps, true).start_driver
+        #Appium.promote_singleton_appium_methods Sample_iOS
+        Appium.promote_appium_methods RSpec::Core::Sample_iOS
+    end
 
-module Sample_iOS
-  module IOS
-    Appium.promote_singleton_appium_methods Sample_iOS
-    #
+    after(:all) do
+      driver_quit
+    end
+
+    describe 'Check Button' do
+        subject { find_elements(:class_name, 'UIButton')[0] }
+        it { should_not be nil }
+        context 'when used as a selection context' do
+          it 'Check title' do
+              expect(it.title).to be '(・ω・)'
+          end
+        end
+    end
+
     el1 = find_element(:accessibility_id, "(・ω・)")
     el1.click
 
@@ -34,8 +53,5 @@ module Sample_iOS
 
     el2 = find_element(:accessibility_id, "◀︎ Sample_iOS")
     el2.click
-    #
-    # driver_quit
-    puts 'Tests Succeeded!'
-  end
+
 end
