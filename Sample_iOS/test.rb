44e82bf3a861732085d5364debce5c1a3fb09a60
WataruSuzuki <wataru0406@gmail.com>
2017-10-25 21:30:00 +0900

    Fixed wrong spec codes

--- Sample_iOS/test.rb
+++ Sample_iOS/test.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'appium_lib'
 require 'rspec'
+require 'spec_helper'
 
 RSpec.configure do |c|
   # c.treat_symbols_as_metadata_keys_with_true_values = true
@@ -11,7 +12,7 @@ APP_PATH = 'appium-unittesting/Debug-iphonesimulator/Sample_iOS.app'
 desired_caps = {
   caps: {
     platformName:  'iOS',
-    platformVersion: '11.1',
+    platformVersion: '11.0',
     deviceName:    'iPhone X',
     app:           APP_PATH,
     automationName: 'Appium',
@@ -26,32 +27,40 @@ desired_caps = {
 describe 'Sample_iOS' do
     before(:all) do
         Appium::Driver.new(desired_caps, true).start_driver
-        #Appium.promote_singleton_appium_methods Sample_iOS
-        Appium.promote_appium_methods RSpec::Core::Sample_iOS
+        Appium.promote_appium_methods RSpec::Core::ExampleGroup
+    end
+
+    def back_click(opts={})
+      opts        ||= {}
+      search_wait = opts.fetch(:wait, 10) # seconds
+      wait(search_wait) { button_exact('Back').click }
     end
 
     after(:all) do
       driver_quit
     end
 
-    describe 'Check Button' do
-        subject { find_elements(:class_name, 'UIButton')[0] }
-        it { should_not be nil }
-        context 'when used as a selection context' do
-          it 'Check title' do
-              expect(it.title).to be '(・ω・)'
-          end
+    describe 'Open browser and back' do
+        before :all do
+            @button = find_element(:accessibility_id, "(・ω・)")
+            @button.click
+
+            def find_id id
+              wait { find_element(:id, "◀︎ Sample_iOS") }
+            end
+
+            iosback = find_element(:accessibility_id, "◀︎ Sample_iOS")
+            iosback.click
+        end
+
+        # Go back to the menu when you're done
+        after :all do
+          # back_click
+        end
+
+        it 'Check button' do
+            @button.displayed?.should be true
+            # expect(subject.title).to be '(・ω・)'
         end
     end
-
-    el1 = find_element(:accessibility_id, "(・ω・)")
-    el1.click
-
-    def find_id id
-      wait { find_element(:id, "◀︎ Sample_iOS") }
-    end
-
-    el2 = find_element(:accessibility_id, "◀︎ Sample_iOS")
-    el2.click
-
 end
